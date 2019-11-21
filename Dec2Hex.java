78ab7e3ffc7dc333c84eacf738768ccdc990aaf1
Talha-A <tahmad202@caledonian.ac.uk>
2019-11-21 17:03:17 +0000

    file is changed

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -19,6 +19,11 @@ class Dec2Hex
                 System.exit(1);
             }
         }
+	else
+	{
+		System.err.println("You must have an input");
+		System.exit(1);
+	}
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
